ea2b5eba72f21136a73264f7ad35bc233bd1d268
Arvie Yonismara <arviyonismara@gmail.com>
2025-02-25 23:47:55 +0700

    Finalize

--- src/App.jsx
+++ src/App.jsx
@@ -141,6 +141,118 @@ function App() {
           </div>
         </div>
       </section>
+
+      {/* References Section */}
+      <section id="references">
+        <div className="flex flex-col items-center justify-between max-x-6xl px-10 mx-auto space-y-16 my-44 md:flex-row md:space-y-0">
+          <img src="images/logo-google.png" alt="" />
+          <img src="images/logo-ibm.png" alt="" />
+          <img src="images/logo-microsoft.png" alt="" />
+          <img src="images/logo-hp.png" alt="" />
+          <img src="images/logo-vector-graphics.png" alt="" />
+        </div>
+      </section>
+
+      {/* Bottom CTA Section */}
+      <section id="bottom">
+        <div className="section-container my-20">
+          <Title>Clipboard for iOS and Mac OS</Title>
+          <p className="section-content text-xl mb-10">
+            Available for free on the App Store. Download for Mac or iOS, sync
+            with iCloud and you're ready to start adding to your clipboard.
+          </p>
+
+          {/* Button Container */}
+          <div className="flex flex-col w-full justify-center space-y-4 md:flex-row md:space-y-0 md:space-x-4">
+            <a
+              href=""
+              className="p-4 px-8 rounded-full shadow-lg bg-strongCyan text-white duration-200 hover:opacity-80"
+            >
+              Download for IOS
+            </a>
+            <a
+              href=""
+              className="p-4 px-8 rounded-full shadow-lg bg-lightBlue text-white duration-200 hover:opacity-80"
+            >
+              Download for Mac
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <footer className="bg-gray-50">
+        <div className="section-container">
+          <div className="flex flex-col items-center justify-between md:flex-row">
+            {/* Image */}
+            <img src="images/logo.svg" alt="" className="scale-50" />
+            {/* Container for menus & Social */}
+            <div className="flex flex-col items-center justify-between flex-1 mb-10 space-y-6 md:flex-row md:mb-0 md:space-y-0 text-grayishBlue">
+              {/* Menus */}
+              <div className="flex flex-col space-y-4 md:flex-row md:ml-24 md:space-x-24 md:space-y-0">
+                {/* Menu 1 */}
+                <div className="flex flex-col space-4 text-center md:text-left">
+                  <div>
+                    <a href="" className="hover:text-strongCyan">
+                      FAQs
+                    </a>
+                  </div>
+                  <div>
+                    <a href="" className="hover:text-strongCyan">
+                      Contact Us
+                    </a>
+                  </div>
+                </div>
+                {/* Menu 2 */}
+                <div className="flex flex-col space-4 text-center md:text-left">
+                  <div>
+                    <a href="" className="hover:text-strongCyan">
+                      Privacy
+                    </a>
+                  </div>
+                  <div>
+                    <a href="" className="hover:text-strongCyan">
+                      Press Kit
+                    </a>
+                  </div>
+                </div>
+                {/* Menu 3 */}
+                <div className="flex flex-col space-4 text-center md:text-left">
+                  <div>
+                    <a href="" className="hover:text-strongCyan">
+                      Install Guide
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              {/* Social Icons */}
+              <div className="flex justify-between w-32 py-1">
+                <a href="">
+                  <img
+                    src="images/icon-facebook.svg"
+                    alt=""
+                    className="duration-200 ficon"
+                  />
+                </a>
+                <a href="">
+                  <img
+                    src="images/icon-twitter.svg"
+                    alt=""
+                    className="duration-200 ficon"
+                  />
+                </a>
+                <a href="">
+                  <img
+                    src="images/icon-instagram.svg"
+                    alt=""
+                    className="duration-200 ficon"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
